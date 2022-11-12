0c71853f3243fd934502e51e327f6eabf1247309
Ana Lozano Moreno <analozanomor@gmail.com>
2022-11-12 19:17:31 +0100

    add fetch

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const urlApi = 'https://breakingbadapi.com/api/characters';
+
+fetch(urlApi)
+  .then((response) => response.json())
+  .then((data) => {
+    console.log('Data from API', data);
+
+    renderAllCharacters(data);
+  });
+
+function renderAllCharacters(data) {
+  let html = '';
+
+  for (const character of data) {
+    const name = character.name;
+    const imgSrc = character.img;
+    const status = character.status;
+    const id = character.char_id;
+
+    console.log(name, imgSrc, status, id);
+  }
+}
